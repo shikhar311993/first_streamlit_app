97654588f59908daf12f2571f0ec91e0632ce1b0
shikhar311993 <68625780+shikhar311993@users.noreply.github.com>
2023-08-17 13:47:47 -0400

    commenting manual code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,15 @@ import pandas
 import streamlit
 import requests
 import snowflake.connector
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show=my_fruit_list.loc[fruits_selected]
+# my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# my_fruit_list = my_fruit_list.set_index('Fruit')
+# # Let's put a pick list here so they can pick the fruit they want to include 
+# fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+# fruits_to_show=my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page.
+# # Display the table on the page.
 
-streamlit.dataframe(fruits_to_show)
+# streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
