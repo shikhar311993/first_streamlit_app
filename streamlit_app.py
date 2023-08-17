3b35c36a6cf8534eb5bb8ac09dc5704503acebe6
shikhar311993 <68625780+shikhar311993@users.noreply.github.com>
2023-08-17 15:15:46 -0400

    Update streamlit_app.py
    Stopping streamlit before snowflake read

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # adding normalized json into a table
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
