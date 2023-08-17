db966d80a5379a5b8c1397f7e8d50f41a8c3f81a
shikhar311993 <68625780+shikhar311993@users.noreply.github.com>
2023-08-17 17:49:57 -0400

    Update streamlit_app.py
    Organizing 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import pandas
 import streamlit
 import requests
 import snowflake.connector
-import urllib.error from URLError
+from urllib.error import URLError
 # my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # my_fruit_list = my_fruit_list.set_index('Fruit')
 # # Let's put a pick list here so they can pick the fruit they want to include 
