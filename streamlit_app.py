7f8c86445e7002e08a4372b75b8ebddd03ce1d83
shikhar311993 <68625780+shikhar311993@users.noreply.github.com>
2023-08-17 15:10:22 -0400

    Update streamlit_app.py
    adding insert statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,4 +33,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfuit')
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('test')")
